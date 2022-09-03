25fed9a28c230fb1226cdf429c4af6ab639249cc
JBetoReyes <josereyes55@hotmail.com>
2022-09-03 09:46:58 -0500

    adds a new file to make a connection against a mongo db

---
+++ src/db/index.js
@@ -0,0 +1,37 @@
+const {MongoClient} = require('mongodb');
+// const {MongoClient} = mongodb;
+
+const {
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD
+} = process.env;
+
+const uri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:27017?retryWrites=true&w=majority&authSource=admin`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+let dbo;
+
+export const connectToServer = async () => {
+    let dbConnection;
+    try {
+        dbConnection = await client.connect();
+    } catch (err) {
+        console.log('error: ', err);
+        return;
+    }
+    dbo = dbConnection.db('quiz');
+    const questions = await dbConnection.db('quiz').collection('questions').find().toArray();
+    console.log('Successfully connected to db');
+}
+
+export const getDbConnection = async () => {
+    if (!dbo) {
+        await connectToServer()
+    }
+    return dbo;
+}
